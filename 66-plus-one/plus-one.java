28f96a52a8aaf697b012a9f87104025f78867b19
saazalll <singhalsajal9528@gmail.com>
2025-07-05 00:40:50 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (66.56%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,18 @@
+class Solution {
+    //
+    public int[] plusOne(int[] digits) {
+        for(int i = digits.length-1; i>=0; i--){
+            if (digits[i] == 9) {
+                digits[i]=0;
+            }
+            else{
+                digits[i]++;
+                return digits;
+                }
+            }
+            int all[] = new int[digits.length+1];
+            all[0]=1;
+            return all;
+              
+    }
+}
